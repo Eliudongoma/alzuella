b858c2f50d717359709f939d986d859770c5a001
augustine-awuori <codewithaugustine@gmail.com>
2024-03-05 14:15:21 +0300

    Rebuild the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+// Box same as <div>, Text same as <p>, <Flex> for <div display='flex'>
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Box>
+      <Text>Main Page! Hello World!</Text>
+    </Box>
+  );
 }
